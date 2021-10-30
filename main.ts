d0be082195bbb64916e6a3c3d93c5ac88a4d65db
DafyddLlyr <dafyddllyr@gmail.com>
2021-10-30 07:12:21 +0100

    Munro details in popup

--- main.ts
+++ main.ts
@@ -1,5 +1,6 @@
 
 import { Feature, Map, View } from 'ol';
+import Overlay from 'ol/Overlay';
 import TileLayer from 'ol/layer/Tile';
 import XYZ from 'ol/source/XYZ';
 import { fromLonLat } from 'ol/proj';
@@ -7,6 +8,9 @@ import VectorSource from 'ol/source/Vector';
 import axios from 'axios';
 import Point from 'ol/geom/Point';
 import VectorLayer from 'ol/layer/Vector';
+import { Coordinate } from 'ol/coordinate';
+import Geometry from 'ol/geom/Geometry';
+import RenderFeature from 'ol/render/Feature';
 
 
 const bgLayer = new TileLayer({
@@ -28,6 +32,34 @@ const map = new Map({
   })
 });
 
+const popup = new Overlay({
+  element: document.getElementById('popup') || undefined
+});
+
+map.on("click", (e) => {
+  map.forEachFeatureAtPixel(e.pixel, (feature, layer) => {
+    setupPopup(e.coordinate, feature);
+    map.addOverlay(popup);
+  })
+});
+
+const setupPopup = (coord: Coordinate, feature: RenderFeature | Feature<Geometry>) => {
+  popup.setPosition(coord);
+  document.getElementById('popup-content');
+  const props = feature.getProperties()
+  const munroDetails = `
+    <div>
+      <h2>${props.name}</h2>
+      <p><b>Height:</b> <span>${props.height}m</span></p>
+      <p><b>Region:</b> <span>${props.region}</span></p>
+      <p><b>Lat Long:</b> <span>[${props.latlng_lat}, ${props.latlng_lng}]</span></p>
+      <p><b>Grid Ref:</b> <span>${props.gridref_letters} ${props.gridref_eastings} ${props.gridref_northings}</span></p>
+      <p><b>Meaning:</b> <span>${props.meaning}m</span></p>
+    </div>
+`
+popup.getElement()!.innerHTML = munroDetails;
+}
+
 const munros = []
 
 const buildMunroFeature = (munro) => (
@@ -37,19 +69,22 @@ const buildMunroFeature = (munro) => (
   })
 )
 
-axios.get("https://munroapi.herokuapp.com/munros").then(
-  response => {
-    response.data.forEach(munro => {
-      const munroFeature = buildMunroFeature(munro)
-      munros.push(munroFeature)
-    })
 
-    const munroLayer = new VectorLayer({
-      source: new VectorSource({
-        features: munros
+
+axios.get("https://munroapi.herokuapp.com/munros")
+  .then(
+    response => {
+      response.data.forEach(munro => {
+        const munroFeature = buildMunroFeature(munro)
+        munros.push(munroFeature)
       })
-    })
 
-    map.addLayer(munroLayer)
+      const munroLayer = new VectorLayer({
+        source: new VectorSource({
+          features: munros
+        })
+      })
 
-  });
+      map.addLayer(munroLayer)
+
+    });
